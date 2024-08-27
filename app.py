4868fc7f7a106f4cb9a1bdd48496eb109612e832
tpoisonooo <khj.application@aliyun.com>
2024-08-27 16:43:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import gradio as gr
 def greet(name):
-    return "Hello" + name + "!!"
+    return "正在处理 https 域名问题，目前可在 http://101.133.161.204:18444/ 体验效果"
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
 iface.launch()
